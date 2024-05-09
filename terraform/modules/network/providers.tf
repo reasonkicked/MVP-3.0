ea17c36dea760d0b38579aec8eb7edc13dbe3622
tomasz.stanislawczyk <tomasz.stanislawczyk@xebia.com>
2024-05-09 16:22:04 +0200

    feature/8-add-github-pipeline-tfstate

--- terraform/modules/network/providers.tf
+++ terraform/modules/network/providers.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = ">= 0.15"
-
-  backend "azurerm" {
-    storage_account_name = "mvp30sadev"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
