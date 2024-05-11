224f95711d9bcc9f185e63209e7d1430ffb32889
tomasz.stanislawczyk <tomasz.stanislawczyk@xebia.com>
2024-05-11 13:27:29 +0200

    feature/12-build-aks-cluster

--- terraform/modules/network/aks.tf
+++ terraform/modules/network/aks.tf
@@ -1,6 +1,29 @@
 module "aks_resource_group" {
   source = "../../modules/bits/resource_group"
 
-  name     = module.conventions.names.kubernetes_cluster.azurerm_resource_group
+  name     = module.conventions.names.aks.azurerm_resource_group
   location = var.location
 }
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = module.conventions.names.aks.kubernetes_cluster
+  location            = var.location
+  resource_group_name = module.aks_resource_group.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
+
