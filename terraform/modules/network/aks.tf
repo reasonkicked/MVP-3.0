5311b59d97e2f72173b439b22414d39f0df702ea
tomasz.stanislawczyk <tomasz.stanislawczyk@xebia.com>
2024-05-22 13:17:35 +0200

    feature/12-build-aks-cluster skip build

--- terraform/modules/network/aks.tf
+++ terraform/modules/network/aks.tf
@@ -42,17 +42,46 @@ resource "azurerm_kubernetes_cluster_node_pool" "node_pool" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
   vm_size               = "Standard_DS2_v2"
   node_count            = 2
-  vnet_subnet_id        = azurerm_subnet.public_subnet.id # Replace with your desired subnet
+  vnet_subnet_id        = azurerm_subnet.public_subnet.id
+}
 
+data "azurerm_key_vault" "key_vault" {
+  name                = "dev-aks-kv"
+  resource_group_name = "ts-rg-01"
+}
+
+data "azurerm_key_vault_secret" "key_vault_secret" {
+  name         = "dev-mgmt-vm"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = module.conventions.names.aks.public_ip
+  location            = var.location
+  resource_group_name = module.aks_resource_group.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = module.conventions.names.aks.nic
+  location            = var.location
+  resource_group_name = module.aks_resource_group.name
+
+  ip_configuration {
+    name                          = module.conventions.names.aks.nic
+    subnet_id                     = azurerm_subnet.public_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
 }
 
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "myVM"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = module.aks_resource_group.name
+  location            = var.location
   size                = "Standard_DS1_v2"
   admin_username      = "adminuser"
-  admin_password      = "P@ssw0rd123!"
+  admin_password      = data.azurerm_key_vault_secret.key_vault_secret.value
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
