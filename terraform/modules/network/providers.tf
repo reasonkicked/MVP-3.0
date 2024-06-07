c96ab5c1271100dd7020933c9a3eb01730381067
tomasz.stanislawczyk <tomasz.stanislawczyk@xebia.com>
2024-06-07 11:01:43 +0200

    feature/12-build-aks-cluster skip build

--- terraform/modules/network/providers.tf
+++ terraform/modules/network/providers.tf
@@ -13,14 +13,4 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_password" "password" {
-  length            = 16
-  special           = true
-  override_special  = "!@#$%&*()-_=+[]{}<>:?"
-}
-variable "db_password" {
-  description = "The password for the database"
-  type        = string
-  sensitive   = true
-}
 
