cdf9a8a6c3d518c689e6452f0faeaf8b5f9ed6ed
reasonkicked <reasonkicked@gmail.com>
2024-12-30 15:17:50 +0100

    k8s vm - splitting up network and kubernetes pipelines

---
+++ terraform/modules/kubernetes/variables.tf
@@ -0,0 +1,28 @@
+
+
+variable "environment" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "application_name" {
+  type    = string
+  default = "wf"
+}
+
+variable "application_instance" {
+  type = string
+}
+
+variable "functions" {
+  type    = set(string)
+  default = []
+}
+
+variable "resource_instance" {
+  type    = number
+  default = "01"
+}
