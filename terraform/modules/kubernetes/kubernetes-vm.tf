99eec73a1d7e9d795784b530590ab062aaa978a7
reasonkicked <reasonkicked@gmail.com>
2024-12-30 15:17:50 +0100

    k8s vm - splitting up network and kubernetes pipelines

--- terraform/modules/kubernetes/kubernetes-vm.tf
+++ terraform/modules/kubernetes/kubernetes-vm.tf
@@ -1,3 +1,23 @@
+data "terraform_remote_state" "network" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "ts-rg-01"
+    storage_account_name = "mvp30backendsa"
+    container_name       = "terraform-states"
+    key                  = "${var.environment}-${var.application_instance}/network.tfstate" # Adjust for environment and instance
+  }
+}
+
+data "azurerm_key_vault" "key_vault" {
+  name                = "dev-aks-kv"
+  resource_group_name = "ts-rg-01"
+}
+
+data "azurerm_key_vault_secret" "key_vault_secret" {
+  name         = "dev-mgmt-vm"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
 resource "azurerm_public_ip" "public_ip" {
   name                = module.conventions.names.aks.public_ip
   location            = var.location
