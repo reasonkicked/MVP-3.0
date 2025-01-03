e10e377b4cdc862e5a18e6a8cbead39f733044f9
reasonkicked <reasonkicked@gmail.com>
2025-01-03 16:10:49 +0100

    extension test

--- terraform/modules/kubernetes/scripts/kubernetes-vm.sh
+++ terraform/modules/kubernetes/scripts/kubernetes-vm.sh
@@ -66,11 +66,6 @@ echo "Installing Docker GPG key and setting up repository..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Add Docker's official GPG key and set up repository
-echo "Installing Docker GPG key and setting up repository..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 # Install and configure containerd
 echo "Installing and configuring containerd..."
 export DEBIAN_FRONTEND=noninteractive
