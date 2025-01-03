efacc2bd6e29e161d3af60a63b6ab45072a6696c
reasonkicked <reasonkicked@gmail.com>
2025-01-03 13:44:45 +0100

    extension test

--- terraform/modules/kubernetes/kubernetes-vm.tf
+++ terraform/modules/kubernetes/kubernetes-vm.tf
@@ -60,7 +60,7 @@ resource "azurerm_virtual_machine_extension" "install_k8s" {
 
   protected_settings = <<PROTECTED_SETTINGS
   {
-    "script": "${base64encode(file("${path.module}/scripts/kubernetes-vm.sh"))}"
+    "script": "${base64encode(file("${path.module}/scripts/k8s-setup.sh"))}"
   }
   PROTECTED_SETTINGS
 }
