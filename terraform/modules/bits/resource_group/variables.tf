770f878a7424209e73483172f5f1e02b434de0b7
reasonkicked <reasonkicked@gmail.com>
2025-01-09 13:21:03 +0100

    extension test

--- terraform/modules/bits/resource_group/variables.tf
+++ terraform/modules/bits/resource_group/variables.tf
@@ -5,9 +5,3 @@ variable "location" {
 variable "name" {
   default = ""
 }
-
-variable "vm_size" {
-  description = "The size of the VM instance."
-  type        = string
-  default     = "Standard_D2ps_v5"
-}
